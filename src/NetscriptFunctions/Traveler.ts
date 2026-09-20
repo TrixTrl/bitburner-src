c57dafe937eff809ed640bd7efd73cd80dfac249
Trix <Trix@DESKTOP-QAEJLQP>
2026-09-20 03:14:42 -0400

    Fix after merge

--- src/NetscriptFunctions/Traveler.ts
+++ src/NetscriptFunctions/Traveler.ts
@@ -10,62 +10,61 @@ export function NetscriptTraveler(): InternalAPI<TravelerAPI> {
     /**
      * Generates a new network with the given number of nodes
      */
-    dev_generateFreshNetwork: (ctx) =>
-      (nodeCount_): void => {
-        const nodeCount = helpers.positiveNumber(ctx, "nodeCount", nodeCount_);
-        Nodes.splice(0);
+    dev_generateFreshNetwork: (ctx, nodeCount_): void => {
+      const nodeCount = helpers.positiveNumber(ctx, "nodeCount", nodeCount_);
+      Nodes.splice(0);
 
-        Nodes.push(
-          {
-            id: Nodes.length,
-            connectionLimit: 5,
-            connectedTo: -1,
-            position: { x: 0, y: 0 },
-            nodeType: "NEXUS",
-            power: 1,
-          } as TravelerNode
-        );
-        while (Nodes.length < nodeCount) {
-          if (Math.random() < 0.1) {
-            Nodes.push(
-              {
-                id: Nodes.length,
-                connectionLimit: 1,
-                connectedTo: -1,
-                position: { x: (Math.random() - .5) * 100, y: (Math.random() - .5) * 100 },
-                nodeType: "MULTIPLIER",
-                power: 1 + Math.random() * 3,
-              } as TravelerNode
-            );
-          } else if (Math.random() < 0.4) {
-            Nodes.push(
-              {
-                id: Nodes.length,
-                connectionLimit: 3 + Math.floor(Math.random() * 4),
-                connectedTo: -1,
-                position: { x: (Math.random() - .5) * 200, y: (Math.random() - .5) * 200 },
-                nodeType: "CONNECTOR",
-                power: 1,
-              } as TravelerNode
-            );
-          } else {
-            Nodes.push(
-              {
-                id: Nodes.length,
-                connectionLimit: 0,
-                connectedTo: -1,
-                position: { x: (Math.random() - .5) * 300, y: (Math.random() - .5) * 300 },
-                nodeType: "PRODUCER",
-                power: Math.random() + Math.random() + Math.random() * 2,
-              } as TravelerNode
-            );
-          }
+      Nodes.push(
+        {
+          id: Nodes.length,
+          connectionLimit: 5,
+          connectedTo: -1,
+          position: { x: 0, y: 0 },
+          nodeType: "NEXUS",
+          power: 1,
+        } as TravelerNode
+      );
+      while (Nodes.length < nodeCount) {
+        if (Math.random() < 0.1) {
+          Nodes.push(
+            {
+              id: Nodes.length,
+              connectionLimit: 1,
+              connectedTo: -1,
+              position: { x: (Math.random() - .5) * 100, y: (Math.random() - .5) * 100 },
+              nodeType: "MULTIPLIER",
+              power: 1 + Math.random() * 3,
+            } as TravelerNode
+          );
+        } else if (Math.random() < 0.4) {
+          Nodes.push(
+            {
+              id: Nodes.length,
+              connectionLimit: 3 + Math.floor(Math.random() * 4),
+              connectedTo: -1,
+              position: { x: (Math.random() - .5) * 200, y: (Math.random() - .5) * 200 },
+              nodeType: "CONNECTOR",
+              power: 1,
+            } as TravelerNode
+          );
+        } else {
+          Nodes.push(
+            {
+              id: Nodes.length,
+              connectionLimit: 0,
+              connectedTo: -1,
+              position: { x: (Math.random() - .5) * 300, y: (Math.random() - .5) * 300 },
+              nodeType: "PRODUCER",
+              power: Math.random() + Math.random() + Math.random() * 2,
+            } as TravelerNode
+          );
         }
-      },
+      }
+    },
     /**
      * Generate a new node of a spesific type
      */
-    dev_generateNewNode: (ctx) => (nodeType): number => {
+    dev_generateNewNode: (ctx, nodeType): number => {
       if (!["PRODUCER", "CONNECTOR", "MULTIPLIER"].find(type => type == nodeType)) throw new Error(`Invalid node type: ${nodeType}`);
       if (nodeType == "MULTIPLIER") {
         Nodes.push(
@@ -106,33 +105,30 @@ export function NetscriptTraveler(): InternalAPI<TravelerAPI> {
     /**
      * Get all nodes
      */
-    getNodes: (ctx) =>
-      (): TravelerNode[] => {
-        return structuredClone(Nodes);
-      },
+    getNodes: (): TravelerNode[] => {
+      return structuredClone(Nodes);
+    },
     /**
      * Links one node to another, returns if the link was succesfull
      */
-    linkNode: (ctx) =>
-      (linkingNodeId_, targetNodeId_): boolean => {
-        const linkingNodeId = helpers.number(ctx, "linkingNodeId", linkingNodeId_);
-        const targetNodeId = helpers.number(ctx, "targetNodeId", targetNodeId_);
-        return linkNode(linkingNodeId, targetNodeId);
-      },
+    linkNode: (ctx, linkingNodeId_, targetNodeId_): boolean => {
+      const linkingNodeId = helpers.number(ctx, "linkingNodeId", linkingNodeId_);
+      const targetNodeId = helpers.number(ctx, "targetNodeId", targetNodeId_);
+      return linkNode(linkingNodeId, targetNodeId);
+    },
     /**
      * Unlinks node from the node it's connected to
      */
-    unlinkNode: (ctx) =>
-      (nodeId_): void => {
-        const nodeId = helpers.number(ctx, "nodeId", nodeId_);
-        if (nodeId > 0 && nodeId < Nodes.length) {
-          Nodes[nodeId].connectedTo = -1;
-        }
-      },
+    unlinkNode: (ctx, nodeId_): void => {
+      const nodeId = helpers.number(ctx, "nodeId", nodeId_);
+      if (nodeId > 0 && nodeId < Nodes.length) {
+        Nodes[nodeId].connectedTo = -1;
+      }
+    },
     /**
      * Unlinks all nodes connected to the passed in node from said node. Returns the number of severed links
      */
-    unlinkIncomming: (ctx) => (nodeId_): number => {
+    unlinkIncomming: (ctx, nodeId_): number => {
       const nodeId = helpers.number(ctx, "nodeId", nodeId_);
       if (nodeId >= 0 && nodeId < Nodes.length) {
         let connections = 0;
@@ -150,7 +146,7 @@ export function NetscriptTraveler(): InternalAPI<TravelerAPI> {
     /**
      * Profiles the power of the network
      */
-    profile: (ctx) => (): number => {
+    profile: (): number => {
       return profileBotnet();
     },
   }
